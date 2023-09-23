9fe11ae19ef8e71e9a51e3f991fc232d053c32ec
Ariel A <arzamendiaariel@gmail.com>
2023-09-23 18:16:23 -0300

    first commit

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LazyImageComponent } from './components/lazyImage/lazy-image.component';
 
 
 
 @NgModule({
   declarations: [
+    LazyImageComponent,
     SidebarComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
+    LazyImageComponent,
     SidebarComponent
   ]
 })
